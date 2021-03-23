b4c15268c588434318b540a93d5bafb069a67554
halprin <halprin@users.noreply.github.com>
2021-03-22 20:36:03 -0600

    Remove commented out shell script code

--- generate_mass_data.sh
+++ generate_mass_data.sh
@@ -6,10 +6,6 @@ table_name="mass-data"
 
 aws dynamodb create-table --table-name "${table_name}" --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --billing-mode PAY_PER_REQUEST --endpoint-url http://127.0.0.1:8002
 
-#for ((index = 0 ; index < num_items ; index++)); do
-#  aws dynamodb put-item --table-name "${table_name}" --item "{\"id\": {\"S\": \"$(uuidgen)\"}}" --endpoint-url http://127.0.0.1:8002
-#done
-
 items_preamble="{\"${table_name}\": ["
 items_middle=""
 items_ending=']}'
