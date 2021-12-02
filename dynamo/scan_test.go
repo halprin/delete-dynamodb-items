90ec104eadbc4556d591c80bfabcaa9579d6e15f
halprin <halprin@users.noreply.github.com>
2021-12-01 20:46:51 -0700

    Get DynamoDB mock set-up with first test

---
+++ dynamo/scan_test.go
@@ -0,0 +1,38 @@
+package dynamo
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+)
+
+func Test_getItemsGoroutine_filterExpressionIsNil(t *testing.T) {
+	mockDynamoDb := ResetDynamoDbMock()
+	mockDynamoDb.On("Scan", mock.Anything).Return(testChannelOfScanMethodReturnType())
+
+	_ = getItemsGoroutine("a table name", nil, nil, nil)
+
+	scanInputArgument := mockDynamoDb.Calls[0].Arguments.Get(0).(*dynamodb.ScanInput)
+
+	assert.Nil(t, scanInputArgument.FilterExpression)
+}
+
+func testChannelOfScanMethodReturnType() chan []map[string]*dynamodb.AttributeValue {
+	channel := make(chan []map[string]*dynamodb.AttributeValue)
+
+	go func() {
+		arrayOfDynamoDbStuff := []map[string]*dynamodb.AttributeValue{
+			{
+				"dogcow": {
+					S: aws.String("moof"),
+				},
+			},
+		}
+		channel <- arrayOfDynamoDbStuff
+		close(channel)
+	}()
+
+	return channel
+}
