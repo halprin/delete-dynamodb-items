e286bfb4cdf60914152c47edaee4148fd33f7987
halprin <halprin@users.noreply.github.com>
2021-12-01 21:13:56 -0700

    Test Scan with expressionAttributeNames

--- dynamo/scan_test.go
+++ dynamo/scan_test.go
@@ -31,6 +31,41 @@ func Test_getItemsGoroutine_filterExpression(t *testing.T) {
 	assert.Equal(t, filterExpression, *scanInputArgument.FilterExpression)
 }
 
+func Test_getItemsGoroutine_expressionAttributeNamesIsNil(t *testing.T) {
+	mockDynamoDb := ResetDynamoDbMock()
+	mockDynamoDb.On("Scan", mock.Anything).Return(testChannelOfScanMethodReturnType())
+
+	_ = getItemsGoroutine("a table name", nil, nil, nil)
+
+	scanInputArgument := mockDynamoDb.Calls[0].Arguments.Get(0).(*dynamodb.ScanInput)
+
+	assert.Nil(t, scanInputArgument.ExpressionAttributeNames)
+}
+
+func Test_getItemsGoroutine_expressionAttributeNamesIsInvalid(t *testing.T) {
+	mockDynamoDb := ResetDynamoDbMock()
+	mockDynamoDb.On("Scan", mock.Anything).Return(testChannelOfScanMethodReturnType())
+	expressionAttributeNames := "not valid JSON"
+
+	goroutine := getItemsGoroutine("a table name", nil, &expressionAttributeNames, nil)
+
+	assert.Nil(t, goroutine)
+	mockDynamoDb.AssertNotCalled(t, "Scan", mock.Anything)
+}
+
+func Test_getItemsGoroutine_expressionAttributeNames(t *testing.T) {
+	mockDynamoDb := ResetDynamoDbMock()
+	mockDynamoDb.On("Scan", mock.Anything).Return(testChannelOfScanMethodReturnType())
+	expressionAttributeNames := `{"dogcow": "moof"}`
+
+	goroutine := getItemsGoroutine("a table name", nil, &expressionAttributeNames, nil)
+
+	scanInputArgument := mockDynamoDb.Calls[0].Arguments.Get(0).(*dynamodb.ScanInput)
+
+	assert.NotNil(t, goroutine)
+	assert.NotNil(t, scanInputArgument.ExpressionAttributeNames)
+}
+
 func testChannelOfScanMethodReturnType() chan []map[string]*dynamodb.AttributeValue {
 	channel := make(chan []map[string]*dynamodb.AttributeValue)
 
