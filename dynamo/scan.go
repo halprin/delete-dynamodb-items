d3254d94bcd680e6296f8b2821f064e016015712
halprin <halprin@users.noreply.github.com>
2024-02-16 20:09:11 -0700

    Test_convertRawAttributeValues_SS works now

--- dynamo/scan.go
+++ dynamo/scan.go
@@ -118,9 +118,18 @@ func convertRawAttributeValues(rawAttributeValue map[string]interface{}) (types.
 
 	value, ok = rawAttributeValue["SS"]
 	if ok {
-		valueListOfStrings, ok := value.([]string)
+		valueList, ok := value.([]interface{})
 		if !ok {
 			return nil, errors.New("SS attribute value is not a list of strings")
+		}
+
+		valueListOfStrings := make([]string, len(valueList))
+		for index, item := range valueList {
+			valueString, ok := item.(string)
+			if !ok {
+				return nil, errors.New("SS attribute value's sub-value is not a string")
+			}
+			valueListOfStrings[index] = valueString
 		}
 
 		return &types.AttributeValueMemberSS{Value: valueListOfStrings}, nil
