e82bc7a259e0fa1bcc0b8e8683e5844722623f6b
halprin <halprin@users.noreply.github.com>
2024-02-16 19:45:09 -0700

    Add remaining attribute value types

--- dynamo/scan.go
+++ dynamo/scan.go
@@ -1,6 +1,7 @@
 package dynamo
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
@@ -71,6 +72,7 @@ func convertRawAttributeValues(rawAttributeValue map[string]interface{}) (types.
 		if !ok {
 			return nil, errors.New("N attribute value is not a string")
 		}
+
 		return &types.AttributeValueMemberN{Value: valueString}, nil
 	}
 
@@ -80,7 +82,113 @@ func convertRawAttributeValues(rawAttributeValue map[string]interface{}) (types.
 		if !ok {
 			return nil, errors.New("S attribute value is not a string")
 		}
+
 		return &types.AttributeValueMemberS{Value: valueString}, nil
+	}
+
+	value, ok = rawAttributeValue["BOOL"]
+	if ok {
+		valueBool, ok := value.(bool)
+		if !ok {
+			return nil, errors.New("BOOL attribute value is not a boolean")
+		}
+
+		return &types.AttributeValueMemberBOOL{Value: valueBool}, nil
+	}
+
+	value, ok = rawAttributeValue["B"]
+	if ok {
+		valueString, ok := value.(string)
+		if !ok {
+			return nil, errors.New("B attribute value is not a string")
+		}
+
+		valueBytes, err := base64.StdEncoding.DecodeString(valueString)
+		if err != nil {
+			return nil, err
+		}
+
+		return &types.AttributeValueMemberB{Value: valueBytes}, nil
+	}
+
+	value, ok = rawAttributeValue["NULL"]
+	if ok {
+		return &types.AttributeValueMemberNULL{}, nil
+	}
+
+	value, ok = rawAttributeValue["SS"]
+	if ok {
+		valueListOfStrings, ok := value.([]string)
+		if !ok {
+			return nil, errors.New("SS attribute value is not a list of strings")
+		}
+
+		return &types.AttributeValueMemberSS{Value: valueListOfStrings}, nil
+	}
+
+	value, ok = rawAttributeValue["NS"]
+	if ok {
+		valueListOfStrings, ok := value.([]string)
+		if !ok {
+			return nil, errors.New("NS attribute value is not a list of strings")
+		}
+
+		return &types.AttributeValueMemberNS{Value: valueListOfStrings}, nil
+	}
+
+	value, ok = rawAttributeValue["BS"]
+	if ok {
+		valueListOfStrings, ok := value.([]string)
+		if !ok {
+			return nil, errors.New("BS attribute value is not a valueListOfBytes of strings")
+		}
+
+		valueListOfBytes := make([][]byte, len(valueListOfStrings))
+		for index, item := range valueListOfStrings {
+			bytes, err := base64.StdEncoding.DecodeString(item)
+			if err != nil {
+				return nil, err
+			}
+
+			valueListOfBytes[index] = bytes
+		}
+
+		return &types.AttributeValueMemberBS{Value: valueListOfBytes}, nil
+	}
+
+	value, ok = rawAttributeValue["L"]
+	if ok {
+		valueList, ok := value.([]map[string]interface{})
+		if !ok {
+			return nil, errors.New("L attribute value is not a list of maps from string to something")
+		}
+
+		list := make([]types.AttributeValue, len(valueList))
+		for index, item := range valueList {
+			attributeValue, err := convertRawAttributeValues(item)
+			if err != nil {
+				return nil, err
+			}
+
+			list[index] = attributeValue
+		}
+
+		return &types.AttributeValueMemberL{Value: list}, nil
+	}
+
+	value, ok = rawAttributeValue["M"]
+	if ok {
+		valueMap, ok := value.(map[string]map[string]interface{})
+		if !ok {
+			return nil, errors.New("M attribute value is not a map")
+		}
+
+		mappedAttributeValues, err := mapAttributeValues(valueMap)
+		if err != nil {
+			return nil, err
+		}
+
+		return &types.AttributeValueMemberM{Value: mappedAttributeValues}, nil
 	}
 
 	return nil, errors.New("attribute value type didn't match any known types")
