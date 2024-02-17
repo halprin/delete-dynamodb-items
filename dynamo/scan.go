18728f520422aaa9a1ee554a7526745354425f33
halprin <halprin@users.noreply.github.com>
2024-02-16 19:25:08 -0700

    Manually parse the attribute values

--- dynamo/scan.go
+++ dynamo/scan.go
@@ -2,6 +2,7 @@ package dynamo
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"log"
@@ -9,6 +10,8 @@ import (
 
 type expressionAttributeNamesType map[string]string
 type expressionAttributeValuesType map[string]types.AttributeValue
+
+//type expressionAttributeValuesType types.AttributeValueMemberM
 
 func getItemsGoroutine(tableName string, filterExpression *string, expressionAttributeNames *string, expressionAttributeValues *string) chan []map[string]types.AttributeValue {
 
@@ -23,9 +26,16 @@ func getItemsGoroutine(tableName string, filterExpression *string, expressionAtt
 
 	var values expressionAttributeValuesType
 	if expressionAttributeValues != nil {
-		err := json.Unmarshal([]byte(*expressionAttributeValues), &values)
+		var mappedData map[string]map[string]interface{}
+		err := json.Unmarshal([]byte(*expressionAttributeValues), &mappedData)
 		if err != nil {
 			log.Printf("Failed to unmarshal the expression attribute values, %+v", err)
+			return nil
+		}
+
+		values, err = mapAttributeValues(mappedData)
+		if err != nil {
+			log.Printf("Failed to map the expression attribute values, %+v", err)
 			return nil
 		}
 	}
@@ -38,4 +48,31 @@ func getItemsGoroutine(tableName string, filterExpression *string, expressionAtt
 	}
 
 	return GetService().Scan(scanInput)
+}
+
+func mapAttributeValues(unmarshaledJson map[string]map[string]interface{}) (expressionAttributeValuesType, error) {
+	expressionAttributeValues := expressionAttributeValuesType{}
+	for key, rawAttributeValue := range unmarshaledJson {
+		attributeValue, err := convertRawAttributeValues(rawAttributeValue)
+		if err != nil {
+			return nil, err
+		}
+		expressionAttributeValues[key] = attributeValue
+	}
+
+	return expressionAttributeValues, nil
+}
+
+func convertRawAttributeValues(rawAttributeValue map[string]interface{}) (types.AttributeValue, error) {
+
+	value, ok := rawAttributeValue["N"]
+	if ok {
+		valueString, ok := value.(string)
+		if !ok {
+			return nil, errors.New("N attribute value is not a string")
+		}
+		return &types.AttributeValueMemberN{Value: valueString}, nil
+	}
+
+	return nil, errors.New("attribute value type didn't match any known types")
 }
--- go.mod
+++ go.mod
@@ -14,10 +14,13 @@ require (
 
 require (
 	github.com/aws/aws-sdk-go-v2/credentials v1.16.16 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.12.17 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression v1.6.17 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.14.11 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.7.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.18.7 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.10.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.8.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.10.10 // indirect
